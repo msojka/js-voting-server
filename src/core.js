5f80b4572b39af093b4a512dbf5fc89294bf5958
Martin Sojka <sojka.martin@gmail.com>
2015-09-17 23:25:25 +0200

    Mark winner as a new state if only one entry left when next-ing.

--- src/core.js
+++ src/core.js
@@ -7,10 +7,29 @@ export function setEntries(state, entries) {
 export function next(state) {
   const entries = state.get('entries')
                         .concat(getWinners(state.get('vote')));
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  if(entries.size === 1) {
+    // We could have just returned
+    // Map({winner: entries.first()}) here.
+    // But instead we still take the old state
+    // as the starting point and explicitly
+    // remove 'vote' and 'entries' keys from it.
+    // The reason for this is future-proofing:
+    // At some point we might have some unrelated
+    // data in the state, and it should pass through
+    // this function unchanged. It is generally
+    // a good idea in these state transformation
+    // functions to always morph the old state
+    // into the new one instead of building
+    // the new state completely from scratch.
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
 
 export function vote(state, entry) {
